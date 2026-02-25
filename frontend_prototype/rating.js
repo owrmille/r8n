c5d5eb17e336e74d44f440700a93b87fa50fc6f7
Mariia Kulikova <mariya.kulikova818@gmail.com>
2026-02-25 18:26:25 +0100

    move frontend prototype from /frontend to /frontend_prototype

---
+++ frontend_prototype/rating.js
@@ -0,0 +1,43 @@
+const details = document.getElementById('details');
+let detailsOpen = false;
+
+document.querySelectorAll('.toggle-details').forEach(cell => {
+  cell.addEventListener('click', e => {
+    e.stopPropagation();
+
+    if (!detailsOpen) {
+      details.classList.remove('hidden');
+      detailsOpen = true;
+    } else {
+      details.classList.add('hidden');
+      detailsOpen = false;
+    }
+  });
+});
+
+const headers = document.querySelectorAll('.th-filter');
+
+  headers.forEach(th => {
+    th.addEventListener('click', e => {
+      e.stopPropagation();
+
+      document.querySelectorAll('.filter-popup').forEach(p => {
+        if (!th.contains(p)) p.classList.remove('open');
+      });
+
+      const popup = th.querySelector('.filter-popup');
+      popup.classList.toggle('open');
+    });
+  });
+
+  document.addEventListener('click', () => {
+    document.querySelectorAll('.filter-popup')
+      .forEach(p => p.classList.remove('open'));
+  });
+
+  const dateInput = document.getElementById('date-filter');
+  if (dateInput) {
+    const d = new Date();
+    d.setMonth(d.getMonth() - 3);
+    dateInput.value = d.toISOString().split('T')[0];
+  }
