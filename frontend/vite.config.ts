e48f7fb71fd6c8372c45d8ef149ff4292366d1f2
Mariia Kulikova <mkulikov@student.42berlin.de>
2026-03-03 18:40:12 +0100

    fix vite proxy

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -14,7 +14,7 @@ export default defineConfig({
   ],
   server: {
     proxy: {
-      '/api': {
+      '^/api(/|$)': {
         target: 'http://localhost:8080',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
