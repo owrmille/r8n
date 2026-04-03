fca3775f4e65d788fa5d3f506813eae679217d37
owrmille <iasemin.topchu@gmail.com>
2026-04-03 17:39:11 +0200

    import users rest client config for UsersInternalApi bean

--- backend/opinions-sv/service/src/main/kotlin/com/r8n/backend/opinions/OpinionsApplication.kt
+++ backend/opinions-sv/service/src/main/kotlin/com/r8n/backend/opinions/OpinionsApplication.kt
@@ -1,9 +1,12 @@
 package com.r8n.backend.opinions
 
+import com.r8n.backend.users.integration.UsersRestClientConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Import
 
 @SpringBootApplication
+@Import(UsersRestClientConfiguration::class)
 class OpinionsApplication
 
 fun main(args: Array<String>) {
