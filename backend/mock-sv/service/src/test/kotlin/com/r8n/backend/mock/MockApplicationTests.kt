712ca474edbe7524386377194563a5542d2dc866
owrmille <iasemin.topchu@gmail.com>
2026-04-21 15:07:17 +0200

    add DB-backed support messaging service layer

--- backend/mock-sv/service/src/test/kotlin/com/r8n/backend/mock/MockApplicationTests.kt
+++ backend/mock-sv/service/src/test/kotlin/com/r8n/backend/mock/MockApplicationTests.kt
@@ -1,12 +1,21 @@
 package com.r8n.backend.mock
 
+import com.r8n.backend.mock.provider.database.SupportMessageRepository
+import com.r8n.backend.mock.provider.database.SupportThreadRepository
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.context.bean.override.mockito.MockitoBean
 
 @ActiveProfiles("test")
 @SpringBootTest
 class MockApplicationTests {
+    @MockitoBean
+    lateinit var supportThreadRepository: SupportThreadRepository
+
+    @MockitoBean
+    lateinit var supportMessageRepository: SupportMessageRepository
+
     @Test
     fun contextLoads() {
     }
